35f300e19b0cdd7e3c3d614fd855c20f89482cbe
David Corkett <dcorkett@gmail.com>
2017-05-19 17:28:59 +0100

    added validation for magnet links; updated version

--- app/js/background.js
+++ app/js/background.js
@@ -6,23 +6,24 @@ chrome.contextMenus.create({
 
 function CopyOnClick(target) {
   var magnetLink = target.linkUrl;
+  if (magnetLink.match(/magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32}/i) !== null) {
 
+    var copyLink = function(url) {
 
-  var copyLink = function(url) {
+      var phantomEl = document.createElement('div');
+      phantomEl.contentEditable = true;
+      document.body.appendChild(phantomEl);
+      phantomEl.innerHTML = 'peerflix ' + '\'' + url.substring(0, url.indexOf('&')) + '\'' + ' --vlc';
+      phantomEl.innerHTML = phantomEl.textContent;
 
-    var phantomEl = document.createElement('div');
-    phantomEl.contentEditable = true;
-    document.body.appendChild(phantomEl);
-    phantomEl.innerHTML = 'peerflix ' + '\'' + url.substring(0, url.indexOf('&')) + '\'' + ' --vlc';
-    phantomEl.innerHTML = phantomEl.textContent;
-
-    phantomEl.unselectable = 'off';
-    phantomEl.focus();
-    document.execCommand('SelectAll');
-    document.execCommand('Copy', false, null);
-    document.body.removeChild(phantomEl);
-  };
-
-  copyLink(magnetLink);
-
+      phantomEl.unselectable = 'off';
+      phantomEl.focus();
+      document.execCommand('SelectAll');
+      document.execCommand('Copy', false, null);
+      document.body.removeChild(phantomEl);
+    };
+    copyLink(magnetLink);
+  }  else {
+    alert('Sorry this isn\'t a valid magnet link');
+  }
 }
